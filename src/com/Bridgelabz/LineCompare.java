e64ef0d039e871677271f6549b466e67c0d50754
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-30 13:45:58 +0530

    Starting Of Line Comparison Problem.

---
+++ src/com/Bridgelabz/LineCompare.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz;
+
+public class LineCompare {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+	}
+}
